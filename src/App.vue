722cabfe92071b943c130355a556af96537a544c
enophan <zhaooooooyifan@qq.com>
2022-05-30 15:25:45 +0800

    chore: update

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,17 @@ const el = ref < HTMLCanvasElement > ()
 onMounted(() => {
   const canvas = el.value!
   const ctx = canvas.getContext('2d')!
+
+  ctx.beginPath() // Start a new path
+  ctx.moveTo(30, 50) // Move the pen to (30, 50)
+  ctx.lineTo(150, 100) // Draw a line to (150, 100)
+  ctx.stroke() // render the line
 })
 </script>
 
 <template>
-  <div>
-    <canvas ref="el" />
-    <div>dnfsbfihdj</div>
+  <div flex="~" items-center justify-center>
+    <canvas ref="el" width="400" height="400" border />
   </div>
 </template>
+

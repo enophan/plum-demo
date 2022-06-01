ff62ecb8d7a1be7c51efa2df569a2280b38529c0
enophan <zhaooooooyifan@qq.com>
2022-06-01 10:54:53 +0800

    chore: update

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,18 @@
 const el = $ref < HTMLCanvasElement > ()
 const ctx = $computed(() => el!.getContext('2d')!)
 
+const WIDTH = 500
+const HEIGHT = 500
+
 function init() {
   ctx.strokeStyle = '#fff' // stghyle shouold on the top
+  Line(WIDTH / 2, HEIGHT, WIDTH / 2, HEIGHT / 2)
 }
 
 function Line(x1: number, y1: number, x2: number, y2: number) {
   ctx.beginPath() // Start a new path
-  ctx.moveTo(30, 50) // Move the pen to (30, 50)
-  ctx.lineTo(150, 100) // Draw a line to (150, 100)
+  ctx.moveTo(x1, y1) // Move the pen to (30, 50)
+  ctx.lineTo(x2, y2) // Draw a line to (150, 100)
   ctx.stroke() // render the line
 }
 

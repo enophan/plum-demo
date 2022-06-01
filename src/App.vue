5108300b73f414b878f1b34988ef4af0d40139a4
enophan <zhaooooooyifan@qq.com>
2022-06-01 17:15:14 +0800

    chore: update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 const el = $ref < HTMLCanvasElement > ()
 const ctx = $computed(() => el!.getContext('2d')!)
-
+// TODO加绿叶和花
 const WIDTH = 500
 const HEIGHT = 500
 
@@ -20,30 +20,51 @@ function init() {
   ctx.strokeStyle = '#fff'
   const b: Branch = {
     startPint: { x: WIDTH / 2, y: HEIGHT },
-    length: 10,
+    length: 12,
     angle: 0,
   }
   step(b)
 }
 
-function step(preBranch: Branch) {
+const pendingTasks: Function[] = []
+
+function step(preBranch: Branch, depth = 0) {
   const startPint: Point = getEndPoint(preBranch)
   drawBranch(preBranch)
-  if (Math.random() < 0.6) {
-    step({
+  if (depth < 3 || Math.random() < 0.5) {
+    pendingTasks.push(() => step({
       startPint,
-      length: preBranch.length,
-      angle: preBranch.angle + 0.2,
-    })
+      length: preBranch.length + (Math.random() * 10 - 5),
+      angle: preBranch.angle + (0.2 * Math.random()),
+    }, depth + 1))
   }
-  if (Math.random() < 0.6) {
-    step({
+  if (depth < 3 || Math.random() < 0.5) {
+    pendingTasks.push(() => step({
       startPint,
-      length: preBranch.length,
-      angle: preBranch.angle - 0.2,
-    })
+      length: preBranch.length + (Math.random() * 10 - 5),
+      angle: preBranch.angle - (0.2 * Math.random()),
+    }, depth + 1))
   }
 }
+
+function frame() {
+  const tasks = [...pendingTasks]
+  pendingTasks.length = 0
+  tasks.forEach(fn => fn())
+}
+
+let frameNumber = 0
+
+function startFrame() {
+  requestAnimationFrame(() => {
+    frameNumber += 1
+    if (frameNumber % 5 === 0)
+      frame()
+    startFrame()
+  })
+}
+
+startFrame()
 
 function drawLine(startPoint: Point, endPoint: Point) {
   ctx.beginPath()
